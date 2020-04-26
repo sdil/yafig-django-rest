d26207839123354a40e7f8e7f1fc4b55043b3eb7
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-04-26 23:19:14 +0800

    Add redoc documentation

--- python-django-rest/yafig_api/urls.py
+++ python-django-rest/yafig_api/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YAFIG API Server",
+        default_version='v1',
+        description='Yet Another Free Instagram Clone API Server (Monolith)',
+    ),
+    public=True
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path("posts/", include("posts.urls")),
 ]
