d31e282c41b95a4e0a9c58c29be2b141e02a0cf3
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-05-17 16:40:53 +0800

    Add refresh token endpoint in URL

--- python-django-rest/user/urls.py
+++ python-django-rest/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from user import api_views
 from rest_framework import permissions
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('', api_views.get_current_user, name="get-current-user"),
     path('login', api_views.TokenObtainPairView.as_view(), name='user-login'),
+    path('refresh', jwt_views.TokenRefreshView.as_view(), name='user-refresh-token'),
     path('register/', api_views.register, name="user-register"),
     path('<str:username>/', api_views.UserDetail.as_view(), name='get-user-detail'),
     path('<str:username>/posts/', api_views.UserPosts.as_view(), name = 'user-post-list'),
